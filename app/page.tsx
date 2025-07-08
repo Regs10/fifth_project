44e29e7ef9da6d440eca5d6f64736df281766ddf
Regs10 <aayushregmi477@gmail.com>
2025-07-08 22:29:17 +0545

    change home page

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <Button>click me</Button>
-      <h1>hello world</h1>
       <Flex>
         <FillButton title="our product" />
         <FillButton bg={false} title="our product" />
